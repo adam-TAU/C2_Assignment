a399d906449c747bd7fbb463195d8e854ab3c21f
adam <adam@adam.adam>
2021-12-30 19:29:00 +0200

    debug structure

--- kmeans.c
+++ kmeans.c
@@ -31,27 +31,250 @@ static void converge(int max_iter);
 
 /* function declarations for supporting the C API */
 static PyObject* fit_capi(PyObject*, PyObject*);
-static double** fit_c(int K_arg, int dim_arg, int num_data_arg, double eps, int max_iter, double** datapoints_arg, int* initial_centroids_indices); /* will most likely have to change that to an extern function */
+static double** fit_c(double eps, int max_iter, double** datapoints_arg, int* initial_centroids_indices); /* will most likely have to change that to an extern function */
 static PyObject* arrayToList_D(double* array, int length);
 static double* listToArray_D(PyObject *list, int length);
 static int* listToArray_I(PyObject *list, int length);
 
 
+/*****************************************************************************/
+
+
+static void assert_other(bool condition) {
+    if(!condition) {
+        puts("An Error Has Occurred");
+        free_program();
+        exit(1);
+    }
+}
+
+/*****************************************************************************/
+
+int dim;
+int num_data;
+dpoint_t *datapoints = NULL;
+
+int K;
+set_t *sets = NULL;
+
+double epsilon;
+
+/*****************************************************************************/
+
+
+/* the fit function. uses its given initialized centroids, datapoints and other cruical data
+ * and performs the kmeans algorithm until convergence
+ */
+static double** fit_c(double epsilon, int max_iter, double** datapoints_arg, int* initial_centroids_indices) {
+	#undef EPSILON
+	#define EPSILON epsilon
+
+	int i;
+	datapoints = calloc(num_data, sizeof(*datapoints));
+	assert_other(NULL != datapoints);
+	
+	for(i = 0; i < num_data; i++) {
+		datapoints[i].data = datapoints_arg[i];
+	}
+
+	
+	/* contaminated */
+	printf("checkpoint 1\n");
+	initialize_sets(initial_centroids_indices);
+	printf("checkpoint 1\n");
+	converge(max_iter);
+	/* contaminated-end */
+	
+	double** centroids_out;
+	centroids_out = (double**)calloc(K, sizeof(double*));
+
+	for (i = 0; i < K; i++) {
+		centroids_out[i] = sets[i].current_centroid.data;
+	}
+	
+	return centroids_out;
+}
+
+
+
+
+/* This function was created to reduce code duplication. It is the engine of the algorithm,
+ * providing the structurization of the clusters until convergence 
+ */
+static void converge(int max_iter) {
+	int iter, i, updated_centroids;
+    for(iter = 0; iter < max_iter; iter++) {
+        for(i = 0; i < num_data; i++) {
+            assign_to_closest(datapoints[i]);
+        }
+
+        updated_centroids = 0;
+        for(i = 0; i < K; i++) {
+            updated_centroids += update_centroid(&sets[i]);
+        }
+
+        if(updated_centroids == 0) { /* Convergence */
+            break;
+        }
+    }
+}
+
+
+
+
+/* Assigns the given datapoint to the closest set that it can find, using the
+ * sqdist function. */
+static void assign_to_closest(dpoint_t dpoint) {
+    int i, min_idx = 0;
+    double min_dist = -1.0;
+
+    for(i = 0; i < K; i++) {
+        double dist = sqdist(sets[i].current_centroid, dpoint);
+
+        if((min_dist < 0.0) || (dist < min_dist)) {
+            min_idx = i;
+            min_dist = dist;
+        }
+    }
+
+    add_to_set(&sets[min_idx], dpoint);
+}
+
+/* Updates the centroid of the given set using its stored `sum` and `count`
+ * properties, while also resetting them to 0 for the next iteration. */
+static int update_centroid(set_t *set) {
+    double dist;
+    int i;
+
+    for(i = 0; i < dim; i++) {
+        set->sum.data[i] /= (double)set->count;
+    }
+
+    dist = sqrt(sqdist(set->sum, set->current_centroid));
+
+    for(i = 0; i < dim; i++) {
+        set->current_centroid.data[i] = set->sum.data[i];
+        set->sum.data[i] = 0.0;
+    }
+
+    set->count = 0;
+
+    return (dist >= EPSILON) ? 1 /* If this set's centroid changed, return 1 */
+                             : 0;
+}
+
+/* Calculates the squared distance between two given datapoints. */
+static double sqdist(dpoint_t p1, dpoint_t p2) {
+    double dot = 0;
+    int i;
+
+    for(i = 0; i < dim; i++) {
+        double temp = p1.data[i] - p2.data[i];
+        dot += temp * temp;
+    }
+
+    return dot;
+}
+
+/* Adds the given datapoint to the provided set, taking into account both the
+ * `sum` and `count` properties. */
+static void add_to_set(set_t *set, dpoint_t dpoint) {
+    int i;
+
+    set->count += 1;
+    for(i = 0; i < dim; i++) {
+        set->sum.data[i] += dpoint.data[i];
+    }
+}
+
+
+
+/* Initializes all of the sets, both allocating memory for the `sum` and
+ * `current_centroid` properties and copying the data from a given index list
+ *  that indicates what datapoint we should initialize the current centroid with.
+ *  In the case of a regular kmeans.c run in HW1, we would power this with the plain [0,1,...,K-1] index list.
+ *  Though, with the kmeans++, we will power it with the observation indices list */
+static void initialize_sets(int* indices) {
+    int i, j;
+
+    sets = calloc(K, sizeof(*sets));
+    assert_other(NULL != sets);
+
+    for(i = 0; i < K; i++) {
+        /* count is already zero. We just need to allocate the centroid and sum
+           datapoints. */
+        init_datapoint(&sets[i].sum);
+        init_datapoint(&sets[i].current_centroid);
+		
+		/* contaminated */
+		printf("checkpoint 1\n");
+        /* Copy initial current_centroid from i-th datapoint */
+        for(j = 0; j < dim; j++) {
+            sets[i].current_centroid.data[j] = datapoints[indices[i]].data[j];
+        }
+        printf("checkpoint 2\n");
+        /* contaminated-end */
+    }
+}
+
+
+
+
+/* Initializes a single datapoint - allocates enough space for it and sets all
+ * the values to zero. */
+static void init_datapoint(dpoint_t *dpoint) {
+    assert_other(dim > 0);
+
+    dpoint->data = calloc(dim, sizeof(*dpoint->data));
+    assert_other(NULL != dpoint->data);
+}
+
+/* Frees the given datapoint. If it's already been freed or not yet allocated,
+ * this function safely does nothing. */
+static void free_datapoint(dpoint_t dpoint) {
+    if(NULL != dpoint.data) {
+        free(dpoint.data);
+    }
+}
+
+/* Frees all of the memory allocated by the program. If a certain variable
+ * hasn't been allocated yet, this function does not attempt to free it. */
+static void free_program() {
+    int i;
+
+    if(NULL != datapoints) {
+        for(i = 0; i < num_data; i++) {
+            free_datapoint(datapoints[i]);
+        }
+        free(datapoints);
+    }
+
+    if(NULL != sets) {
+        for(i = 0; i < K; i++) {
+            free_datapoint(sets[i].current_centroid);
+            free_datapoint(sets[i].sum);
+        }
+        free(sets);
+    }
+}
+
+
+
+
 /************************* configuring the C API ****************************************************/
 
-static double epsilon;
 
 /******************************************************************************/
 
 static PyObject* fit_capi(PyObject *self, PyObject *args) {
-	int K_arg, dim_arg, num_data_arg, max_iter;
+	int max_iter;
 	double** centroids_c;
 	PyObject *centroids_py;
 	PyObject *result;
 	PyObject *initial_centroids_py;
 	PyObject *datapoints_py;
 
-	if(!PyArg_ParseTuple(args, "iiidiO!O!", &K_arg, &dim_arg, &num_data_arg, &epsilon, &max_iter, &PyList_Type, &datapoints_py, &PyList_Type, &initial_centroids_py)) {
+	if(!PyArg_ParseTuple(args, "iiidiO!O!", &K, &dim, &num_data, &epsilon, &max_iter, &PyList_Type, &datapoints_py, &PyList_Type, &initial_centroids_py)) {
 		return NULL;
 	}
 	
@@ -61,20 +284,20 @@ static PyObject* fit_capi(PyObject *self, PyObject *args) {
 	double** datapoints_arg;
 	int* initial_centroids_indices;
 
-	datapoints_arg = (double**)calloc(num_data_arg, sizeof(double*));
-	for (i = 0; i < num_data_arg; i++) {
-		datapoints_arg[i] = listToArray_D(PyList_GetItem(datapoints_py, i), dim_arg);
+	datapoints_arg = (double**)calloc(num_data, sizeof(double*));
+	for (i = 0; i < num_data; i++) {
+		datapoints_arg[i] = listToArray_D(PyList_GetItem(datapoints_py, i), dim);
 	}
 	
-	initial_centroids_indices = listToArray_I(initial_centroids_py, K_arg);
+	initial_centroids_indices = listToArray_I(initial_centroids_py, K);
 
 	/* building the returned centroids' list */
 	/* segmentation fault occurs in the following line (lol) */
-	centroids_c = fit_c(K_arg, dim_arg, num_data_arg, epsilon, max_iter, datapoints_arg, initial_centroids_indices);
-	centroids_py = PyList_New(num_data_arg);
+	centroids_c = fit_c(epsilon, max_iter, datapoints_arg, initial_centroids_indices);
+	centroids_py = PyList_New(num_data);
 
-	for(i = 0; i < num_data_arg; i++) {
-		PyList_Append(centroids_py, arrayToList_D(centroids_c[i], dim_arg));
+	for(i = 0; i < num_data; i++) {
+		PyList_Append(centroids_py, arrayToList_D(centroids_c[i], dim));
 	}
 
 	result = Py_BuildValue("O", centroids_py);
@@ -200,216 +423,6 @@ PyInit_mykmeanssp(void) {
 }
 
 
-/*****************************************************************************/
 
-
-static void assert_other(bool condition) {
-    if(!condition) {
-        puts("An Error Has Occurred");
-        free_program();
-        exit(1);
-    }
-}
-
-/*****************************************************************************/
-
-int dim = 0;
-int num_data = 0;
-dpoint_t *datapoints = NULL;
-
-int K = 0;
-set_t *sets = NULL;
-
-/*****************************************************************************/
-
-
-/* the fit function. uses its given initialized centroids, datapoints and other cruical data
- * and performs the kmeans algorithm until convergence
- */
-static double** fit_c(int K_arg, int dim_arg, int num_data_arg, double epsilon, int max_iter, double** datapoints_arg, int* initial_centroids_indices) {
-	#undef EPSILON
-	#define EPSILON epsilon /* the stem of the issue */
-
-	int i;
-	dim = dim_arg;
-	num_data = num_data_arg;
-	datapoints = calloc(num_data, sizeof(*datapoints));
-	assert_other(NULL != datapoints);
-	
-	for(i = 0; i < num_data; i++) {
-		datapoints[i].data = datapoints_arg[i];
-	}
-	
-	initialize_sets(initial_centroids_indices);
-	converge(max_iter);
-	
-	double** centroids_out;
-	centroids_out = (double**)calloc(K, sizeof(double*));
-
-	for (i = 0; i < K; i++) {
-		centroids_out[i] = sets[i].current_centroid.data;
-	}
-	
-	return centroids_out;
-}
-
-
-
-
-/* This function was created to reduce code duplication. It is the engine of the algorithm,
- * providing the structurization of the clusters until convergence 
- */
-static void converge(int max_iter) {
-	int iter, i, updated_centroids;
-    for(iter = 0; iter < max_iter; iter++) {
-        for(i = 0; i < num_data; i++) {
-            assign_to_closest(datapoints[i]);
-        }
-
-        updated_centroids = 0;
-        for(i = 0; i < K; i++) {
-            updated_centroids += update_centroid(&sets[i]);
-        }
-
-        if(updated_centroids == 0) { /* Convergence */
-            break;
-        }
-    }
-}
-
-
-
-
-/* Assigns the given datapoint to the closest set that it can find, using the
- * sqdist function. */
-static void assign_to_closest(dpoint_t dpoint) {
-    int i, min_idx = 0;
-    double min_dist = -1.0;
-
-    for(i = 0; i < K; i++) {
-        double dist = sqdist(sets[i].current_centroid, dpoint);
-
-        if((min_dist < 0.0) || (dist < min_dist)) {
-            min_idx = i;
-            min_dist = dist;
-        }
-    }
-
-    add_to_set(&sets[min_idx], dpoint);
-}
-
-/* Updates the centroid of the given set using its stored `sum` and `count`
- * properties, while also resetting them to 0 for the next iteration. */
-static int update_centroid(set_t *set) {
-    double dist;
-    int i;
-
-    for(i = 0; i < dim; i++) {
-        set->sum.data[i] /= (double)set->count;
-    }
-
-    dist = sqrt(sqdist(set->sum, set->current_centroid));
-
-    for(i = 0; i < dim; i++) {
-        set->current_centroid.data[i] = set->sum.data[i];
-        set->sum.data[i] = 0.0;
-    }
-
-    set->count = 0;
-
-    return (dist >= EPSILON) ? 1 /* If this set's centroid changed, return 1 */
-                             : 0;
-}
-
-/* Calculates the squared distance between two given datapoints. */
-static double sqdist(dpoint_t p1, dpoint_t p2) {
-    double dot = 0;
-    int i;
-
-    for(i = 0; i < dim; i++) {
-        double temp = p1.data[i] - p2.data[i];
-        dot += temp * temp;
-    }
-
-    return dot;
-}
-
-/* Adds the given datapoint to the provided set, taking into account both the
- * `sum` and `count` properties. */
-static void add_to_set(set_t *set, dpoint_t dpoint) {
-    int i;
-
-    set->count += 1;
-    for(i = 0; i < dim; i++) {
-        set->sum.data[i] += dpoint.data[i];
-    }
-}
-
-
-
-/* Initializes all of the sets, both allocating memory for the `sum` and
- * `current_centroid` properties and copying the data from a given index list
- *  that indicates what datapoint we should initialize the current centroid with.
- *  In the case of a regular kmeans.c run in HW1, we would power this with the plain [0,1,...,K-1] index list.
- *  Though, with the kmeans++, we will power it with the observation indices list */
-static void initialize_sets(int* indices) {
-    int i, j;
-
-    sets = calloc(K, sizeof(*sets));
-    assert_other(NULL != sets);
-
-    for(i = 0; i < K; i++) {
-        /* count is already zero. We just need to allocate the centroid and sum
-           datapoints. */
-        init_datapoint(&sets[i].sum);
-        init_datapoint(&sets[i].current_centroid);
-
-        /* Copy initial current_centroid from i-th datapoint */
-        for(j = 0; j < dim; j++) {
-            sets[i].current_centroid.data[j] = datapoints[indices[i]].data[j];
-        }
-    }
-}
-
-
-
-
-/* Initializes a single datapoint - allocates enough space for it and sets all
- * the values to zero. */
-static void init_datapoint(dpoint_t *dpoint) {
-    assert_other(dim > 0);
-
-    dpoint->data = calloc(dim, sizeof(*dpoint->data));
-    assert_other(NULL != dpoint->data);
-}
-
-/* Frees the given datapoint. If it's already been freed or not yet allocated,
- * this function safely does nothing. */
-static void free_datapoint(dpoint_t dpoint) {
-    if(NULL != dpoint.data) {
-        free(dpoint.data);
-    }
-}
-
-/* Frees all of the memory allocated by the program. If a certain variable
- * hasn't been allocated yet, this function does not attempt to free it. */
-static void free_program() {
-    int i;
-
-    if(NULL != datapoints) {
-        for(i = 0; i < num_data; i++) {
-            free_datapoint(datapoints[i]);
-        }
-        free(datapoints);
-    }
-
-    if(NULL != sets) {
-        for(i = 0; i < K; i++) {
-            free_datapoint(sets[i].current_centroid);
-            free_datapoint(sets[i].sum);
-        }
-        free(sets);
-    }
-}
 
 
